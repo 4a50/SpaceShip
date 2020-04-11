39be05baf112df83103fe91e46fb9f4ecdf2d615
4a50 <jpjones22@gmail.com>
2020-04-11 12:17:32 -0700

    Not the master one

--- modulesServer.py
+++ modulesServer.py
@@ -1,6 +1,6 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 2222))
+s.bind(('', 2222))
 s.listen(5)
 while True:
     clientsocket, address = s.accept()
