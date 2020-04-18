8bda020e60b1fdfe2f0262f1ad8365231d4664ec
4a50 <jpjones22@gmail.com>
2020-04-17 23:02:06 -0700

    Beginning to work on having the indicating needle progress through an arc.

--- indicatorWidgets.py
+++ indicatorWidgets.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import cmath
+import math
 import time
 def Main():
     gridYes = False
@@ -13,6 +14,12 @@ def Main():
     
     canvX = 400
     canvY = 400
+    x = canvX / 2
+    y = canvY /2
+    centerxy = x, y
+    needleRadius = x * .4
+    needleangle = 90
+    print ("Needle Angle Radians:", math.radians(needleangle))
     guageWidth = canvX * .15
     startArc = 0
     canvEdgeUL = 0.2
@@ -28,24 +35,25 @@ def Main():
             ind1Canv.create_line(0,i, canvX, i)
             ind1Canv.create_line(i,0,i, canvY,)
 
+    #TODO: Figure the equation to move a needle along an ellipse 
     
-    
-    
-    
-    #ind1Canv.create_arc(0, 0, 100, 100, width=6,start=0,extent=180)
-   
-    
-    redSeg = ind1Canv.create_arc(arcCoord, start=300,
-    extent=120,width=guageWidth, outline="red", style=ARC)
-    yellowSeg = ind1Canv.create_arc(arcCoord, start=startArc+1,
+    yellowSeg = ind1Canv.create_arc(arcCoord, start=359,
     extent=119,width=guageWidth, outline="yellow", style=ARC)
-    GreenSeg = ind1Canv.create_arc(arcCoord, start=startArc+120,
-    extent=120,width=guageWidth, outline="green", style=ARC)
-    needleCenter = ind1Canv.create_arc(arcCoord, start=0, extent=359, width=2)
+    redSeg = ind1Canv.create_arc(arcCoord, start=300,
+    extent=59,width=guageWidth, outline="red", style=ARC)
+    time.sleep(1)
     ind1Canv.update()
+    GreenSeg = ind1Canv.create_arc(arcCoord, start=startArc+118,
+    extent=122,width=guageWidth, outline="green", style=ARC)
+    IndCenterLine = ind1Canv.create_arc(arcCoord, start=300, extent=300, width=2, style=ARC)
+    ind1Canv.update()
+
+    Needle_line = ind1Canv.create_line(centerxy, needleRadius * math.cos(needleangle), needleRadius * math.sin(needleangle))
+    print (needleRadius * math.cos(needleangle), (needleRadius * math.sin(needleangle)))
+
     
     
-    # for i in range(0, 60, 6):
+    # for i in range(0, 60, 6)
     #     ind1Canv.create_arc(canvX * canvEdgeUL, canvY * canvEdgeUL, canvX * canvEdgeBR, canvY * canvEdgeBR, start=startArc+(240 - i), 
     #     extent=-1, width=guageWidth/2, style=ARC, outline="black")
     #     ind1Canv.create_arc(canvX * canvEdgeUL, canvY * canvEdgeUL, canvX * canvEdgeBR, canvY * canvEdgeBR, start=startArc+ (180 - i),
